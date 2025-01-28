bfb258cc1f0e2d041b4c4aeee9ac7a911ec2d589
sinyaam <novagg43@gmail.com>
2025-01-28 11:32:05 +0200

    Add files via upload

--- js/vormscript.js
+++ js/vormscript.js
@@ -8,8 +8,29 @@ function lugemine2() {
     let nimi = document.getElementById("nimi").value;
     let pnimi = document.getElementById("pnimi").value;
     let vastus = document.getElementById("vastus");
-    let varv = document.getElementById("vavr");
+    let varv = document.getElementById("varv");
+    let mees =document.getElementById("mees");
+    let naine=document.getElementById("naine");
+    let paev= document.getElementById("synnipaev").value;
+    let GBRAM=document.getElementById("GBRAM").value;
+    let email=document.getElementById("email").value;
+    let tel=document.getElementById("tel").value;
+    let url=document.getElementById("url").value;
 
-    vastus.innerHTML ="Tere hommikust, "+nimi+" "+pnimi+"!";
-    vastus.style.color =varv.value;
+    if (mees.checked) {
+        vastus.innerHTML ="Tere hommikust, mees "+nimi+" "+pnimi+"!"+
+        " Sinu sünnipäev on "+paev+"<br>GB RAM-i teil on:"+GBRAM+"<br>teil email on:"
+            +email+"<br>teil tel number on:"+tel+"<br>url, mille kirjutasid:"+url;
+        vastus.style.color =varv.value;
+
+    }
+    else if (naine.checked) {
+        vastus.innerHTML ="Tere hommikust, naine "+nimi+" "+pnimi+"!"+
+        " Sinu sünnipäev on "+paev+"<br>GB RAM-i teil on:"+GBRAM+"<br>teil email on:"
+            +email+"<br>teil tel number on:"+tel+"<br>url, mille kirjutasid:"+url;
+        vastus.style.color =varv.value;
+    }
+    else {
+        vastus.innerHTML ="täitke andmed"
+    }
 }
